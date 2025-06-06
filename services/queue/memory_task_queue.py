546355707e52054e7c508f60e1e6b72361e6b0a0
Клабуков Эрик Константинович <klabukov@skbkontur.ru>
2025-06-07 00:03:20 +0500

    Fixes after review

--- services/queue/memory_task_queue.py
+++ services/queue/memory_task_queue.py
@@ -1,17 +1,21 @@
-from services.queue.task_queue import TaskQueue
 import threading
+import logging
+from services.queue.task_queue import TaskQueue
 
 class InMemoryTaskQueue(TaskQueue):
     def __init__(self):
         self._queue = []
         self._lock = threading.Lock()
+        self.logger = logging.getLogger(InMemoryTaskQueue.__name__)
 
     def enqueue(self, task):
         with self._lock:
             self._queue.append(task)
+            self.logger.debug("Task enqueued %s", task.__class__.__name__)
 
     def dequeue(self):
         with self._lock:
             if not self._queue:
+                self.logger.debug("Queue is empty")
                 return None
             return self._queue.pop(0)
--- worker.py
+++ worker.py
@@ -10,7 +10,7 @@ from services.review_service import ReviewService
 
 class Worker(threading.Thread):
     def __init__(self, gitea_service: GiteaService, review_service: ReviewService, queue: TaskQueue):
-        super().__init__(None, None, None, None, None, daemon=True)
+        super().__init__(daemon=True)
         self.gitea_service = gitea_service
         self.review_service = review_service
         self.queue = queue
@@ -32,13 +32,13 @@ class Worker(threading.Thread):
     def __process_gitea_review(self, review_task: ReviewTask) -> None:
         try:
             pull_request = GiteaPrUrl.create_from_url(review_task.pull_request_url)
-            self.logger.info(f"At first, get diff from repo {pull_request.owner}/{pull_request.repo} PR#{pull_request.pr_number}")
+            self.logger.info(f"Start review {pull_request.owner}/{pull_request.repo} #{pull_request.pr_number}")
             diff = self.gitea_service.get_pr_diff(pull_request)
-            self.logger.info(f"Send diff to LLM for review")
+            self.logger.info("Send diff to LLM for review")
             review_batch = self.review_service.review_pull_request(diff, review_task.user_message)
-            self.logger.info(f"Publish review in comments")
             for review in review_batch:
                 self.gitea_service.post_comment(pull_request, review)
             self.logger.info("Review completed")
         except Exception as e:
             self.logger.error(f"Error during review process for PR #{pull_request.pr_number}: {e}", exc_info=True)
+            time.sleep(60 * 10) # Wait longer after an error
